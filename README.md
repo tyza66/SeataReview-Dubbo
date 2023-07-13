96c730f261ad62b4705f356b12c51e0007285449
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-07-13 08:55:00 +0800

    更新

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # SeataReview-Dubbo
 ##### Seata重温-Dubbo
 - 在一个分布式项目中遇到了Seata和Dubbo一起使用的情况，并且出现了分布式事务失效的现象，于是就有了这个实验
-- 实验结果：可以使用，但是需要注意的是，Seata的版本、Dubbo的版本、Mybatis的版本、SpringBoot版本都要匹配才可以
+- 实验目的：检验Seata环境是否配置正确
+- 实验环境：Nacos、Seata、Dubbo、Mybatis、SpringBoot
+- 实验结果：可以使用，但是需要注意的是，Nacos版本、Seata的版本、Dubbo的版本、Mybatis的版本、SpringBoot版本都要匹配才可以
 
 By：tyza66
---
+++ 数据库/one.sql
@@ -0,0 +1,57 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80033
+ Source Host           : localhost:3306
+ Source Schema         : one
+
+ Target Server Type    : MySQL
+ Target Server Version : 80033
+ File Encoding         : 65001
+
+ Date: 12/07/2023 19:26:43
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for one
+-- ----------------------------
+DROP TABLE IF EXISTS `one`;
+CREATE TABLE `one`  (
+  `id` int NOT NULL,
+  `num` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of one
+-- ----------------------------
+INSERT INTO `one` VALUES (1, 9);
+
+-- ----------------------------
+-- Table structure for undo_log
+-- ----------------------------
+DROP TABLE IF EXISTS `undo_log`;
+CREATE TABLE `undo_log`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint NOT NULL,
+  `xid` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `context` varchar(128) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `ux_undo_log`(`xid`, `branch_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of undo_log
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
